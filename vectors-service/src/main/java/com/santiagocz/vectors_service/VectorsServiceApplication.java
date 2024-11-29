4b884674a9a0d7fcc5f70a3cb1da0b854f9a24f2
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-11-29 08:51:01 -0300

    Feat: add Vector service with initial setup and configurations

---
+++ vectors-service/src/main/java/com/santiagocz/vectors_service/VectorsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.santiagocz.vectors_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class VectorsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorsServiceApplication.class, args);
+	}
+
+}
---
+++ vectors-service/src/test/java/com/santiagocz/vectors_service/VectorsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagocz.vectors_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
