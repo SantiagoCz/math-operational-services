093fbdc15ef2815462921e98e2336d802c083545
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-11-29 08:47:11 -0300

    Initial commit: setup API Gateway, Config Server, and Eureka Server with basic configurations

---
+++ config-server/src/test/java/com/santiagocz/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagocz.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-server/src/test/java/com/santiagocz/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagocz.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
