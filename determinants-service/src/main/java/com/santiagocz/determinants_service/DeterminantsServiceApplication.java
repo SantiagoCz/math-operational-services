35e90d7451b9c4ea18ac976473a7324ae04a690e
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-12-08 21:23:00 -0300

    feat: add EnableFeignClients annotation to main class for enabling Feign clients

--- determinants-service/src/main/java/com/santiagocz/determinants_service/DeterminantsServiceApplication.java
+++ determinants-service/src/main/java/com/santiagocz/determinants_service/DeterminantsServiceApplication.java
@@ -3,9 +3,11 @@ package com.santiagocz.determinants_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class DeterminantsServiceApplication {
 
 	public static void main(String[] args) {
