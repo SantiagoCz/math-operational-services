ba00aa8924a313cae8f1bf20c80650deccf8d8eb
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-12-08 21:19:51 -0300

    feat: integrate DeterminantClient for determinant, adjugate, and inverse matrix calculations

--- matrices-service/src/main/java/com/santiagocz/matrices_service/services/MatrixService.java
+++ matrices-service/src/main/java/com/santiagocz/matrices_service/services/MatrixService.java
@@ -1,5 +1,6 @@
 package com.santiagocz.matrices_service.services;
 
+import com.santiagocz.matrices_service.clients.DeterminantClient;
 import com.santiagocz.matrices_service.model.Matrix;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,12 @@ import java.util.Objects;
 
 @Service
 public class MatrixService {
+
+    private final DeterminantClient determinantClient;
+
+    public MatrixService(DeterminantClient determinantClient) {
+        this.determinantClient = determinantClient;
+    }
 
     public String defineMatrix() {
         return "";
@@ -26,9 +33,7 @@ public class MatrixService {
     }
 
     public Matrix add(Matrix... matrices) {
-
         verifyDimensionForEachMatrix(matrices);
-
         double[][] resultant = new double[matrices[0].getRows()][matrices[0].getColumns()];
 
         for (Matrix matrix : matrices) {
@@ -42,7 +47,6 @@ public class MatrixService {
     }
 
     public Matrix multiplyByScalar(Matrix matrix, double scalar) {
-
         double[][] resultant = new double[matrix.getRows()][matrix.getColumns()];
 
         for (int i = 0; i < matrix.getRows(); i++) {
@@ -50,14 +54,11 @@ public class MatrixService {
                 resultant[i][j] = scalar * matrix.getMatrix()[i][j];
             }
         }
-
         return new Matrix(resultant);
     }
 
     public Matrix multiply(Matrix matrix1, Matrix matrix2) {
-
         verifyConditionForMultiplicate(matrix1, matrix2);
-
         double[][] resultant = new double[matrix1.getRows()][matrix2.getColumns()];
 
         for(int i = 0; i < matrix1.getRows(); i++) {
@@ -68,12 +69,33 @@ public class MatrixService {
                 for(int k = 0; k < matrix1.getColumns(); k++) {
                     acumulator += matrix1.getMatrix()[i][k] * matrix2.getMatrix()[k][j];
                 }
-
                 resultant[i][j] = acumulator;
             }
         }
-
         return new Matrix(resultant);
+    }
+
+    public double getDeterminant(Matrix matrix) {
+        verifyIfSquareMatrix(matrix);
+        return determinantClient.calculateDeterminant(matrix);
+    }
+
+    public Matrix getAdjugateMatrix(Matrix matrix) {
+        verifyIfSquareMatrix(matrix);
+        return determinantClient.calculateAdjugateMatrix(matrix);
+    }
+
+    public Matrix calculateInverse(Matrix matrix) {
+        double determinant = getDeterminant(matrix);
+
+        if(determinant == 0){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "No se puede calcular inversa, el determinante de la matriz es igual a 0.");
+        } else {
+            Matrix adjugateMatrix = getAdjugateMatrix(matrix);
+            Matrix transposedMatrix = getTransposedMatrix(adjugateMatrix);
+            double scalar = 1/determinant;
+            return multiplyByScalar(transposedMatrix, scalar);
+        }
     }
 
     //-------------------------------------------------------------------------------------------
@@ -86,10 +108,16 @@ public class MatrixService {
             }
         }
     }
+
+    private void verifyIfSquareMatrix(Matrix matrix) {
+        if (matrix.getRows() != matrix.getColumns()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "La matriz debe ser cuadrada.");
+        }
+    }
+
     private void verifyConditionForMultiplicate(Matrix matrix1, Matrix matrix2){
         if(matrix1.getColumns() != matrix2.getRows()) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "No hay coincidencias entre cantidad de columnas y filas.");
         }
     }
-
 }
